56e1ec3afa1400d7d9e04387013598ac627955f6
Johann Soetbeer <johann.soetbeer@gmail.com>
2026-05-21 20:19:44 +0200

    implemented symbol_dictionary logic

--- include/backtest-cpp/symbol_dictionary.h
+++ include/backtest-cpp/symbol_dictionary.h
@@ -1,14 +1,15 @@
+#pragma once
 #include <cstdint>
-#include <unordered_map>>
+#include <unordered_map>
 #include <string>
 
 class symbol_dictionary {
     public:
         uint32_t get_id(const std::string& symbol);
-        std::string get_smbol(uint32_t id) const;
+        std::string get_symbol(uint32_t id) const;
 
     private:
-    std::unordered_map<std::string, uint32_t> string_to_id;
-    std::unordered_map<uint32_t, std::string> id_to_string;
-    uint32_t next_id = 0;
+        std::unordered_map<std::string, uint32_t> string_to_id;
+        std::unordered_map<uint32_t, std::string> id_to_string;
+        uint32_t next_id = 0;
 }; 
